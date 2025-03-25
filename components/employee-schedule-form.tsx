43125bc89e32515e7bbe76f644fbc0e822799d9e
Macqy Garcia <garciamacqy@gmail.com>
2025-03-25 22:19:24 +0800

    Added missing icon imports

--- components/employee-schedule-form.tsx
+++ components/employee-schedule-form.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/components/ui/select';
 import { DatePickerWithRange } from './date-picker-with-range';
 import { Switch } from '@/components/ui/switch';
+import { PlusCircle, Save, Trash2 } from 'lucide-react';
 
 export function EmployeeScheduleForm() {
 	const [selectedEmployee, setSelectedEmployee] = React.useState('');

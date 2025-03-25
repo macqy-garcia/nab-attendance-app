2d117d065e882bd2690e20abdee530cd2871388e
Macqy Garcia <garciamacqy@gmail.com>
2025-03-25 22:15:42 +0800

    Remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import { DatePickerWithRange } from '@/components/date-picker-with-range';
 import { EmployeeScheduleForm } from '@/components/employee-schedule-form';
-import { LoginForm } from '@/components/login-form';
-import { SignUpForm } from '@/components/sign-up-form';
-import Image from 'next/image';
 
 export default function Home() {
 	return (
--- components/employee-schedule-form.tsx
+++ components/employee-schedule-form.tsx
@@ -1,26 +1,10 @@
 'use client';
 
 import * as React from 'react';
-import { format } from 'date-fns';
-import { CalendarIcon, PlusCircle, Save, Trash2 } from 'lucide-react';
-
-import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
-import { Calendar } from '@/components/ui/calendar';
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from '@/components/ui/popover';
 import {
 	Select,
 	SelectContent,
